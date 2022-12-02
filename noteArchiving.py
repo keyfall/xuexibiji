4560b95e9c37065fd1c04da3080beb91d77f6c94
ememem <2536726426@qq.com>
2022-12-02 09:13:45 +0800

    noteArchiving.py
    修改版，之前用文件名匹配，现在使用修改时间去匹配

--- noteArchiving.py
+++ noteArchiving.py
@@ -8,30 +8,27 @@ import shutil
 file_address = "D:\学习笔记\日记"
 target_address = file_address+"\复习"
 
-def change_day(d):
-    ld = d.split('.')
-    if 10 > int(ld[0]):
-        ld[0]=ld[0][-1]
-    if 10 > int(ld[1]):
-        ld[1] = ld[1][-1]
-    return '.'.join(ld)
-
 now = datetime.datetime.now()
 all_files = os.listdir(file_address)
 
+#删除昨天生成的目录
+if os.path.exists(target_address):
+    shutil.rmtree(target_address)
 
 
-one = change_day((now - datetime.timedelta(days=1)).strftime("%m.%d"))+'.docx'
-two = change_day((now - datetime.timedelta(days=2)).strftime("%m.%d"))+'.docx'
-four = change_day((now - datetime.timedelta(days=4)).strftime("%m.%d"))+'.docx'
-seven = change_day((now - datetime.timedelta(days=7)).strftime("%m.%d"))+'.docx'
-fifteen = change_day((now - datetime.timedelta(days=15)).strftime("%m.%d"))+'.docx'
+
+one = (now - datetime.timedelta(days=1)).strftime("%m.%d")
+two = (now - datetime.timedelta(days=2)).strftime("%m.%d")
+four = (now - datetime.timedelta(days=4)).strftime("%m.%d")
+seven = (now - datetime.timedelta(days=7)).strftime("%m.%d")
+fifteen = (now - datetime.timedelta(days=15)).strftime("%m.%d")
 
 
 files = [one,two,four,seven,fifteen]
 
-for x in iter(files):
-    if x in all_files:
+#遍历文件修改日期，然后和对应的天数生成的日期进行对比，符合就新建目录，copy文件进去
+for x in iter(all_files):
+    if datetime.datetime.fromtimestamp(os.path.getmtime(file_address+'\\'+x)).strftime("%m.%d") in files:
         os.makedirs(target_address, exist_ok=True)
         shutil.copy(file_address+'\\'+x,target_address)
 
