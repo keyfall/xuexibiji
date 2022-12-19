44e2c35f729fa12a1890573df877293ab4de2129
ememem <2536726426@qq.com>
2022-12-19 12:41:41 +0800

    增加日期回调
    增加了一个日期回调功能，就是因为可能有事耽误了，就没有学习，中间过了几天没学习，可以填写间隔日子，然后正常学习
    
    增加了这个功能后，每次运行都要进行填写间隔日期，如果没有，那就直接回车，默认是0天
    
    有这个日期回调功能，是因为我阳了，躺了几天，没法学习，想看又不记得从哪里看起，就整了一个这个功能 
    当时针对这个日期回调想了两个办法，一个是手动输入，一个就是每次复习文档生成后记录日期，后续打开这个日期+1即可，不管实际日期，这种缺点第一是固定日期，不能随意调换日期，第二个是因为会在电脑本地增加一个存储文档，记录日期，相当于一个setting文件

--- noteArchiving.py
+++ noteArchiving.py
@@ -5,10 +5,27 @@ import shutil
 
 # 根据艾宾浩斯记忆曲线，把文档日期在1,2,4,7,15天的文件存放到复习文件夹中,每天早上进行复习
 
+# 输入数字,来实现几天没有学习，算实际复习日期=当前日期-几天空闲
+def delay_days_process(point_for_input):
+    delay_days = input(point_for_input)
+    if not delay_days:
+        return 0
+    if not delay_days.isdigit():
+        delay_days = delay_days_process("输入数字:")
+    return int(delay_days)
+
+
+delay_days = delay_days_process("你丫几天没学习了（输入天数）:")
+
+
+now = datetime.datetime.now() - datetime.timedelta(days=delay_days)
+
+
+
 file_address = "D:\学习笔记\日记"
 target_address = file_address+"\复习"
 
-now = datetime.datetime.now()
+
 all_files = os.listdir(file_address)
 
 #删除昨天生成的目录
@@ -22,9 +39,12 @@ two = (now - datetime.timedelta(days=2)).strftime("%m.%d")
 four = (now - datetime.timedelta(days=4)).strftime("%m.%d")
 seven = (now - datetime.timedelta(days=7)).strftime("%m.%d")
 fifteen = (now - datetime.timedelta(days=15)).strftime("%m.%d")
+onemonth = (now - datetime.timedelta(days=30)).strftime("%m.%d")
+fourtyfive = (now - datetime.timedelta(days=45)).strftime("%m.%d")
+sixty = (now - datetime.timedelta(days=60)).strftime("%m.%d")
 
 
-files = [one,two,four,seven,fifteen]
+files = [one,two,four,seven,fifteen,onemonth,fourtyfive,sixty]
 
 #遍历文件修改日期，然后和对应的天数生成的日期进行对比，符合就新建目录，copy文件进去
 for x in iter(all_files):
